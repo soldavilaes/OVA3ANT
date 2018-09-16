3ec9418abd1813e587b7b9b8987da0722ce84e6d
Marisol Davila Escobar <marisol.davila@correounivalle.edu.co>
2018-09-16 11:54:28 -0500

    INTRODUCCIÓN

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ $('.menu-left').on('click', function() {
 	$('.intro').toggleClass('abrir');
 });*/
 
+/*------------------------------------
+* EFECTOS DEL MENU
+--------------------------------------*/
+
 $(function() {
   var Accordion = function(el, multiple) {
     this.el = el || {};
@@ -89,3 +93,32 @@ $(document).ready(function() {
             
         })();
 
+/*------------------------------------
+* EFECTO DE ENTRADA DE LA INTRODUCCÓN
+--------------------------------------*/
+
+$(document).ready(function() {
+  /* top-left logo animation */
+    $('.logo-container')
+      .stop(true)
+      .animate({
+      left:'30px'
+    }, 1000);
+  $('h2')
+        .stop(true)
+        .animate({
+            opacity:"1"
+        },1200);
+  /* title animation */
+  $('h1')
+        .stop(true)
+        .animate({
+            opacity:"1"
+        },1500);
+  /* top-left logo animation */
+  $('.rectangle1') 
+        .stop(true)
+        .animate({
+            left:"99%",  
+        },2200);
+});
