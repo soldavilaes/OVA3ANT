f3e10afa6babb1fdc50ed99a59e5a3c0745f0ae6
pacovi0606 <40521162+pacovi0606@users.noreply.github.com>
2018-09-19 01:25:30 -0500

    pestaña 4
    step 2

--- tema3.js
+++ tema3.js
@@ -158,3 +158,25 @@ $('.button2').click(function(){
 $('.button3').click(function(){    
   $(this).toggleClass("buttonDisplay3");
 });
+
+/*-----------------------------
+* LIGHTBOX
+-------------------------------*/
+
+$(document).ready(function () {
+    "use strict";
+    $(".lightbox").click(function () {
+        var imgsrc = $(this).attr('src');
+        $("body").append("<div class='img-popup'><span class='close-lightbox'>&times;</span><img src='" + imgsrc + "'></div>");
+        $(".close-lightbox, .img-popup").click(function () {
+            $(".img-popup").fadeOut(500, function () {
+                $(this).remove();
+            }).addClass("lightboxfadeout");
+        });
+
+    });
+    $(".lightbox").click(function () {
+        $(".img-popup").fadeIn(500);
+    });
+
+});
