0706d66361d7f86f468ad9096feacfb62b354f89
Marisol Davila Escobar <marisol.davila@correounivalle.edu.co>
2018-09-22 01:24:50 -0500

    PESTAÑA 3 - ITEM 1

--- tema3.js
+++ tema3.js
@@ -206,3 +206,13 @@ $(function() {
   //END
   
 });
+
+var selector = document.querySelector(".toggle");
+
+selector.addEventListener("click", function() {
+  if (this.classList.contains("active")) {
+    this.className = "toggle";
+  } else {
+    this.className += " active";
+  }
+});
