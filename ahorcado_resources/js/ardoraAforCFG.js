0aa1c74d60f802c1abbe0e560da11e3e16c2aa3c
pacovi0606 <40521162+pacovi0606@users.noreply.github.com>
2018-10-03 11:08:40 -0500

    act1

---
+++ ahorcado_resources/js/ardoraAforCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=1; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=0
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#FF8080"; colorText="#000000"; colorSele="#0080C0";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="'Arial Black', Gadget, sans-serif";
+var fActi="'Arial Black', Gadget, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=2; messageOk="Bien hecho!"; messageTime=""; messageError="Selecciona otra letra"; messageErrorG="Vuelve a intentarlo"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="YWhvcmNhZG8"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["UEVSRElEQSBERUwgQ09OT0NJTUlFTlRP","","","","","","","","",""];imaW=["","","","","","","","","",""];queW=["Es considerada un problema que ocurre en la evaluación recursiva de funciones estructurales como funciones de procesamiento de listas.","","","","","","","","",""];
+var auW=["","","","","","","","","",""];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ "; wordsG=[]; indexG=0; actMaxWidth=300; indexWord=0; totalWord=0;
+var imageW=[];questionW=[];audioW=[];profG=0; dirMedia="ahorcado_resources/media/"; textBNext="";
